f4b584e0b16a3ab3e00b380b2429129948bda21c
Eduardo Fernandes <iamedusm@gmail.com>
2023-10-10 15:11:47 -0300

    #54 criação da tela de relatorios

--- screens/Administrador/Pages/DetailsScreen.js
+++ screens/Administrador/Pages/DetailsScreen.js
@@ -1,16 +1,105 @@
-//This is an example code for Bottom Navigation//
 import React from 'react';
-//import react in our code.
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
-//import all the basic component we have used
 
 export default class DetailsScreen extends React.Component {
-  //Detail Screen to show from any Open detail button
   render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>RELATORIOS</Text>
+    const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#EBEBEB',
+      paddingHorizontal: 16,
+    },
+    titulo: {
+      fontSize: 30,
+      textAlign: 'center',
+      lineHeight: 30,
+      borderBottomWidth: 1,
+      paddingTop: 60,
+      paddingBottom: 20,
+    },
+    dados: {
+      alignItems: 'center',
+      paddingVertical: 25,
+    },
+    pc: {
+      paddingTop: 20,
+      color: '#0D5C63',
+      fontWeight: '350',
+      fontSize: 60,
+      lineHeight: 50,
+    },
+    pcDescription: {
+      fontSize: 20,
+      color: '#4B4848',
+      fontWeight: '300',
+    },
+    vcm: {
+      fontSize: 24,
+      color: '#0D5C63',
+    },
+    vcmNumber: {
+      fontSize: 48,
+    },
+    vcmDescription: {
+      fontSize: 20,
+      lineHeight: 20,
+      color: '#4B4848',
+      fontWeight: '300',
+    },
+    ltp: {
+      fontSize: 22,
+      color: '#0D5C63',
+    },
+    ltpNumber: {
+      fontSize: 40,
+    },
+    ltpDescription:{
+      fontSize: 18,
+      color: '#4B4848',
+      fontWeight: '300',
+    },
+    lpv:{
+      fontSize: 18,
+      color: '#0D5C63',
+    },
+    lpvNumber:{
+      fontSize: 36,
+    },
+    lpvDescription:{
+      fontSize: 16,
+      color: '#4B4848',
+      fontWeight: '300',
+    }
+  });
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>{'Relatório\n de Vendas'}</Text>
+      <View style={styles.dados}>
+        <Text style={styles.pc}>356</Text>
+        <Text style={styles.pcDescription}>Produtos Cadastrados</Text>
       </View>
-    );
+      <View style={styles.dados}>
+        <Text style={styles.vcm}>
+          R$<Text style={styles.vcmNumber}>128.791</Text>,63
+        </Text>
+        <Text style={styles.vcmDescription}>
+          {'Valor de custo\nda mercadoria'}
+        </Text>
+      </View>
+      <View style={styles.dados}>
+        <Text style={styles.ltp}>
+          R$<Text style={styles.ltpNumber}>40.671</Text>,29
+        </Text>
+        <Text style={styles.ltpDescription}>Lucro total previsto</Text>
+      </View>
+      <View style={styles.dados}>
+        <Text style={styles.lpv}>
+          R$<Text style={styles.lpvNumber}>608</Text>,90
+        </Text>
+        <Text style={styles.lpvDescription}>Lucro p/ Venda</Text>
+      </View>
+    </View>
+  );
   }
 }
