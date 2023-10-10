7f6b8ed14e50f2c2e62a4bb09a52f708ffeaf0b6
unknown <2214290063@ACADEMICO.IESB.BR>
2023-10-10 20:37:29 -0300

    #22 criação do link e da rota de recuperar senha

--- screens/Login/Login.js
+++ screens/Login/Login.js
@@ -1,10 +1,44 @@
 import { View, Text, TextInput, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 import styles from '../../styles/login/Style';
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 // component
 const Login = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const [emailError, setEmailError] = useState('');
+  const [senhaError, setSenhaError] = useState('');
+
+  const handleSubmit = () => {
+    console.log(email.search('@'));
+    console.log(email)
+    let isValid = true;
+
+    // Validação do e-mail
+    if (!email.trim()) {
+      isValid = false;
+      setEmailError('Preencha o campo de e-mail corretamente');
+    } else {
+      setEmailError('');
+    }
+
+    // Validação da senha
+    if (!senha.trim()) {
+      isValid = false;
+      setSenhaError('O campo de senha é obrigatorio');
+    } else {
+      setSenhaError('');
+    }
+
+    if (isValid) {
+      navigation.navigate('Main');
+    }
+  };
+
   return (
+    
     <View style={styles.container}>
       <Image
         source={require('../../assets/img/login/login_topo_cinza.png')}
@@ -21,16 +55,30 @@ const Login = ({ navigation }) => {
         <TextInput
           style={styles.input}
           placeholder="Email"
+          onChangeText={(text) => setEmail(text)}
           keyboardType="email-adress"
         />
+        {emailError ? <Text style={{ color: 'red' }}>{emailError}</Text> : null}
 
-        <TextInput style={styles.input} placeholder="Senha" secureTextEntry />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          onChangeText={(text) => setSenha(text)}
+          secureTextEntry
+        />
+        {senhaError ? <Text style={{ color: 'red' }}>{senhaError}</Text> : null}
+
+        <TouchableOpacity onPress={() => navigation.navigate('RecuperarSenha')}>
+          <Text style={styles.recSenha}>Esqueci minha senha</Text>
+        </TouchableOpacity>
 
         <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('Main')}>
+          onPress={() => {
+            handleSubmit();
+          }}>
           <Text style={styles.buttonText}>Entrar</Text>
-          <FontAwesome name="arrow-right" size={15} color="white"/>
+          <FontAwesome name="arrow-right" size={15} color="white" />
         </TouchableOpacity>
 
         <Text style={styles.titleCadastro}>É o seu primeiro acesso?</Text>
